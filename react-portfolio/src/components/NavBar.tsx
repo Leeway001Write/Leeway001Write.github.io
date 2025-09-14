1cfdb05e4ccb1e063d43983e4fcd0b25823eeb41
Leeway001Write <ginjaninjahex@gmail.com>
2025-09-13 21:17:50 -0600

    Reordered nav bar

--- react-portfolio/src/components/NavBar.tsx
+++ react-portfolio/src/components/NavBar.tsx
@@ -5,6 +5,8 @@ export default function NavBar() {
   return (
     <div className="nav-bar">
       <ul className="nav-list">
+        <li><a className="nav-heading" href="#top">Resume</a></li>
+        <br />
         <li><a className="nav-heading" href="#home"><b>Home</b></a></li>
         <li><a className="nav-heading" href="#about-me"><b>About Me</b></a></li>
         <li><a className="nav-heading" href="#experience"><b>Experience</b></a>
@@ -14,7 +16,7 @@ export default function NavBar() {
             <li><a className="nav-subheading" href="#coursework">Coursework</a></li>
           </ul>
         </li>
-        <li><a className="nav-heading" href="#top">Resume</a></li>
+        <br />
         <li><a className="nav-heading" href="#contact"><b>Contact Me</b></a></li>
       </ul>
     </div>
